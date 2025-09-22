2d235aa1a093a48848f2b2471a8802cd35bf48ba
Daniel Smedema <7870631+djsmedes@users.noreply.github.com>
2025-09-21 22:21:36 -0500

    improvements

--- generate_project_group_transactions.js
+++ generate_project_group_transactions.js
@@ -4,58 +4,63 @@ import fs from 'fs';
 const STUDENTS = JSON.parse(fs.readFileSync('enriched_CMS_Project_Groups.secret.json'));
 const SEED = fs.readFileSync('seed.secret').toString().trim();
 
-const GROUP_TRANSACTIONS = new Map();
+const SQL_BY_GROUP = new Map();
 
 STUDENTS.forEach(student => {
   const netid = student.netId;
-  const email = student.email;
   const password = crypto.createHmac("SHA256", SEED).update(netid).digest("hex").substring(0, 16);
-  const display_name = `${student.first} ${student.last}`;
 
-  const existing_group = GROUP_TRANSACTIONS.get(student.group) ?? [];
+  const existing_group = SQL_BY_GROUP.get(student.group) ?? [];
 
-  // thanks ChatGPT for the SQL
-  const transaction = `
-START TRANSACTION;
-
--- 1) Insert user into wp_users
+  const creation_sql = `
 INSERT INTO wp_users (
     user_login,
-    user_pass,
     user_nicename,
     user_email,
-    user_url,
-    user_registered,
-    user_activation_key,
-    user_status,
-    display_name
+    display_name,
+    user_pass,
+    user_registered
 ) VALUES (
     '${netid}', -- user_login
+    '${student.first.toLowerCase()}-${student.last.toLowerCase()}', -- user_nicename, used as a slug in URLs
+    '${student.email}', -- user_email
+    '${student.first} ${student.last}', -- display_name
     MD5('${password}'), -- user_pass (WordPress will rehash on login)
-    '${netid}', -- user_nicename
-    '${email}', -- user_email
-    '', -- user_url
-    NOW(), -- user_registered
-    '', -- user_activation_key
-    0, -- user_status
-    '${display_name}' -- display_name
+    NOW() -- user_registered
 );
 
--- 2) Capture the ID of the user we just created
 SET @new_user_id = LAST_INSERT_ID();
 
--- 3) Recommend password change
 INSERT INTO wp_usermeta (
     user_id,
     meta_key,
     meta_value
 ) VALUES (
     @new_user_id,
-    'default_password_nag',
-    '1'
+    'nickname',
+    '${student.first}'
 );
 
--- 4) Assign user role
+INSERT INTO wp_usermeta (
+    user_id,
+    meta_key,
+    meta_value
+) VALUES (
+    @new_user_id,
+    'first_name',
+    '${student.first}'
+);
+
+INSERT INTO wp_usermeta (
+    user_id,
+    meta_key,
+    meta_value
+) VALUES (
+    @new_user_id,
+    'last_name',
+    '${student.last}'
+);
+
 INSERT INTO wp_usermeta (
     user_id,
     meta_key,
@@ -66,13 +71,41 @@ INSERT INTO wp_usermeta (
     'a:1:{s:13:"administrator";b:1;}'
 );
 
-COMMIT;
+-- Recommend password change
+INSERT INTO wp_usermeta (
+    user_id,
+    meta_key,
+    meta_value
+) VALUES (
+    @new_user_id,
+    'default_password_nag',
+    '1'
+);
+
+-- Enable block editor by default
+INSERT INTO wp_usermeta (
+    user_id,
+    meta_key,
+    meta_value
+) VALUES (
+    @new_user_id,
+    'rich_editing',
+    'true'
+);
 
 `
-  GROUP_TRANSACTIONS.set(student.group, [...existing_group, transaction])
+  SQL_BY_GROUP.set(student.group, [...existing_group, creation_sql])
 })
 
-for (const [group, transaction_array] of GROUP_TRANSACTIONS.entries()) {
-  const output = [`USE cs272_wp_P${group};\n`, ...transaction_array].join("\n");
+for (const [group, sql_array] of SQL_BY_GROUP.entries()) {
+  const output = [
+    `USE cs272_wp_P${group};\n`, 
+    `START TRANSACTION;\n`, 
+    ...sql_array,
+    // note we specifically DON'T put "COMMIT;" in here:
+    //   when we copy and paste this in, manually confirm there are no errors
+    //   before manually entering "COMMIT;"
+  ].join("\n");
+  
   fs.writeFileSync(`project_group_transactions/p${group}.sql`, output);
 }
